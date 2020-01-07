39d1a4be46f2664b349f744cc690fb5150afa015
Benjono <56357931+Benjono@users.noreply.github.com>
2020-01-07 18:03:39 +0000

    Finished Function

--- bottomUpHeap.py
+++ bottomUpHeap.py
@@ -1,8 +1,34 @@
 def bottomUpHeap(heap):
     #takes list as  input
     newHeap = []
-    for i in range(len(heap)-1,-1,-1):
-        print(heap[i])
-    return newHeap
-bottomUpHeap([0,1,2,3,4,5])
+    currentItem = len(heap)-1
+    while currentItem>=0:
+        if(currentItem<(len(heap)-1)/2):
+            bubbled=False;
+            currentPlace = currentItem
+            while(not bubbled and currentPlace<len(heap)/2):
+                print(heap)
+                if heap[currentPlace*2+1]>heap[currentPlace] or heap[currentPlace*2+2]>heap[currentPlace]:
+                    #if either children are larger
+                    if heap[currentPlace*2+1]>heap[currentPlace*2+2]:
+                        #choose which one
+                        temp = heap[currentPlace]
+                        heap[currentPlace]=heap[currentPlace*2+1]
+                        heap[currentPlace*2+1]=temp
+                        #perform swap
+                        currentPlace=currentPlace*2+1
+                        #move pointer
+                    else:
+                        temp = heap[currentPlace]
+                        heap[currentPlace]=heap[currentPlace*2+2]
+                        heap[currentPlace*2+2]=temp
+                        #perform swap
+                        currentPlace=currentPlace*2+2
+                        #move pointer
+                else:
+                    bubbled=True;
+                    #if the children are less than it, then it's in the right place
+        currentItem-=1
+    return heap
+print(bottomUpHeap([0,1,2,3,4,5,6]))
 
